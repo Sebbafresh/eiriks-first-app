3bbe0574376ad6ae3313eea4e6aa2845ec979bf5
sebastian <ereon@live.com>
2022-05-12 21:58:38 +0200

    Co-authored-by: eirikroe@live.no <eirikroe@live.no>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-app';
+
+  public lagOppsett: string[] = ["Ronaldo","Eirik", "Messi", "Holding", "Haaland", "Oedegaard", "Xhaka", "Kane", "Saka"];
+  
+
+  public ngAfterViewInit(){
+  
+    
+    this.byttInnSpiller("Ronaldo", 1);
+
+    
+
+  }
+
+
+  // Denne metoden skal sette innbytteren inn på en viss plass i lagOppsett
+  public byttInnSpiller(innbytter: string, plassILagoppsett: number){
+    // this.lagOppsett.forEach((spillerNavn, plass) => {
+    //   if (plassILagoppsett == plass){
+    //     this.lagOppsett[plassILagoppsett] = innbytter;
+    //   }
+
+    // })
+
+    this.lagOppsett[plassILagoppsett] = innbytter;
+      
+    console.log("Dette er laget nå: ", this.lagOppsett);
+
+  }
+
+  
+
+  public spillFotballKamp(maalLag1: number, maalLag2: number){
+    if(maalLag1 > maalLag2) {
+      console.log("Arsenal suger")
+    } else {
+      console.log("Xhaka player of the year, idiott")
+    }
+    
+  }
+ 
+
+  
 }
